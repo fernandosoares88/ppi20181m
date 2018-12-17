6f54050e156133cef88a66d8e47b36e66dc03686
1060782 <1060782@MC266447.ifrn.local>
2018-12-17 10:23:01 -0300

    Adicionando filtro com Enconding correto no Serlevt do Spring.

--- agendaspring/src/agendaspring/config/ServletSpringMVC.java
+++ agendaspring/src/agendaspring/config/ServletSpringMVC.java
@@ -1,5 +1,8 @@
 package agendaspring.config;
 
+import javax.servlet.Filter;
+
+import org.springframework.web.filter.CharacterEncodingFilter;
 import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
 
 public class ServletSpringMVC extends AbstractAnnotationConfigDispatcherServletInitializer {
@@ -13,13 +16,20 @@ public class ServletSpringMVC extends AbstractAnnotationConfigDispatcherServletI
 	@Override
 	protected Class<?>[] getServletConfigClasses() {
 		// TODO Auto-generated method stub
-		return new Class[] {AppWebConfiguration.class};
+		return new Class[] { AppWebConfiguration.class };
 	}
 
 	@Override
 	protected String[] getServletMappings() {
 		// TODO Auto-generated method stub
-		return new String[] {"/"};
+		return new String[] { "/" };
+	}
+
+	@Override
+	protected Filter[] getServletFilters() {
+		CharacterEncodingFilter encodingFilter = new CharacterEncodingFilter();
+		encodingFilter.setEncoding("UTF-8");
+		return new Filter[] { encodingFilter };
 	}
 
 }
